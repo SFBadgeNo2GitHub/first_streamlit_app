be4db33eeb39b13c35befc56f494f0d6962bab7e
MellyMel Melryk <121886816+SFBadgeNo2GitHub@users.noreply.github.com>
2023-01-31 15:22:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,12 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
- 
+
+# import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🥣 Omega 3 & Black and Blue Berries Oatmeal')
